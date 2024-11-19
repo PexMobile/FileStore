6e7ff947e7039c75458bec0f2b0f4e9af837f8e3
PexMobile <fun4ugaming2@gmail.com>
2024-11-19 22:40:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -46,8 +46,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "ʜᴇʟʟᴏ {first}\n\n<b>ᴊᴏɪɴ ᴏᴜʀ ɪɴꜱᴛᴀɢʀᴀᴍ :- https://www.instagram.com/hanimeverse/
- ᴀɴᴅ ᴊᴏɪɴ ᴏᴜʀ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴛʜᴇɴ ᴄʟɪᴄᴋ ᴏɴ ɪ ʜᴀᴠᴇ ᴊᴏɪɴᴇᴅ ᴜᴘ ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ʀᴇQᴜᴇꜱᴛᴇᴅ ꜰɪʟᴇ.</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "ʜᴇʟʟᴏ {first}\n\n<b>ᴊᴏɪɴ ᴏᴜʀ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴛʜᴇɴ ᴄʟɪᴄᴋ ᴏɴ ɪ ʜᴀᴠᴇ ᴊᴏɪɴᴇᴅ ᴜᴘ ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ʀᴇQᴜᴇꜱᴛᴇᴅ ꜰɪʟᴇ.</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>• ʙʏ @OtakuFlix_Network</b>")
